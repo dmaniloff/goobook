29cd216d4c2781baf646e44b5d8367508f00ce64
Christer Sjöholm <hcs@furuvik.net>
2010-02-20 12:36:47 +0000

    Added license

--- goobook.py
+++ goobook.py
@@ -1,11 +1,27 @@
 #!/usr/bin/env python
 # vim: fileencoding=UTF-8 filetype=python ff=unix expandtab sw=4 sts=4 tw=120
 # maintainer: Christer Sjöholm -- goobook AT furuvik DOT net
+#
+# Copyright (C) 2009  Carlos José Barroso
+# Copyright (C) 2010  Christer Sjöholm
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 '''
 The idea is make an interface to google contacts that mimics the behaviour of
 abook for mutt. It's developed in python and uses the fine
 google data api (gdata).
-
 '''
 
 import sys
