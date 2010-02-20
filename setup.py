28fb02d1a983301f76c6c78be59218d9cf387dbc
Christer Sjöholm <hcs@furuvik.net>
2010-02-20 20:59:57 +0000

    Fixed indent

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(name='goobook',
       maintainer = u'Christer Sjöholm',
       maintainer_email = 'goobook@furuvik.net',
       url = 'http://goobook.googlecode.com/',
-
-     classifiers = [f.strip() for f in """
+      classifiers = [f.strip() for f in """
         Development Status :: 5 - Production/Stable
         Environment :: Console
         Operating System :: OS Independent
