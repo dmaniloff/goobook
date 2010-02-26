5fcb5ad135f4c225ceb00580cbfe0a49726f8676
Christer Sjöholm <hcs@furuvik.net>
2010-02-26 19:20:46 +0000

    Added option to specify other configfile.

--- goobook.py
+++ goobook.py
@@ -27,6 +27,7 @@ google data api (gdata).
 import codecs
 import email.header
 import locale
+import optparse
 import sys
 import os
 import re
@@ -41,7 +42,7 @@ from gdata.data import Email, Name, FullName
 #from gdata.contacts.service import ContactsService, ContactsQuery
 #import atom
 
-CONFIG_PATH = '~/.goobookrc'
+CONFIG_FILE = '~/.goobookrc'
 CONFIG_EXAMPLE = '''[DEFAULT]
 # If not given here, email and password is taken from .netrc using
 # machine google.com
@@ -249,17 +250,7 @@ class AbookError(Exception):
     '''Exception thrown when failing to parse a abook file.'''
     pass
 
-def usage():
-    print """\
-Usage: goobook.py <command> [<arg>]
-Commands:
-   reload
-   query <name>
-   add <mail.at.stdin>\
-"""
-    sys.exit(1)
-
-def read_config():
+def read_config(config_file):
     ''' Reads the ~/.goobookrc and ~/.netrc.
         returns the configuration as a dictionary.
     '''
@@ -270,14 +261,14 @@ def read_config():
         'cache_filename': '~/.goobook_cache',
         'cache_expiry_hours': '24',
         }
-    if os.path.lexists(CONFIG_PATH):
+    if os.path.lexists(config_file):
         try:
-            configfile = ConfigParser.SafeConfigParser()
-            configfile.readfp(open(os.path.expanduser(CONFIG_PATH)))
-            config.update(dict(configfile.items('DEFAULT', raw=True)))
+            parser = ConfigParser.SafeConfigParser()
+            parser.readfp(open(os.path.expanduser(config_file)))
+            config.update(dict(parser.items('DEFAULT', raw=True)))
         except (IOError, ConfigParser.ParsingError):
             print >> sys.stderr, "Failed to read %s\n\nExample:\n\n%s" % (
-                CONFIG_PATH, CONFIG_EXAMPLE)
+                config_file, CONFIG_EXAMPLE)
             sys.exit(1)
     if not config.get('email') or not config.get('password'):
         auth = netrc().authenticators('google.com')
@@ -292,21 +283,42 @@ def read_config():
 
 
 def main():
-    if len(sys.argv) < 2:
-        usage()
-    config = read_config()
+
+    class MyParser(optparse.OptionParser):
+        def format_epilog(self, formatter):
+            return self.epilog
+    usage = 'usage: %prog [options] <command> [<arg>]'
+    description = 'Search you Google contacts from mutt or the command-line.'
+    epilog = '''\
+Commands:
+  add <mail.at.stdin>
+  reload
+  query <name>
+
+'''
+    parser = MyParser(usage=usage, description=description, epilog=epilog)
+    parser.set_defaults(config_file=CONFIG_FILE)
+    parser.add_option("-c", "--config", dest="config_file",
+                    help="Specify alternative configuration file.", metavar="FILE")
+    (options, args) = parser.parse_args()
+    if len(args) == 0:
+        parser.print_help()
+        sys.exit(1)
+    config = read_config(options.config_file)
     goobk = GooBook(config)
-    if sys.argv[1] == "query":
-        if len(sys.argv) < 3:
-            usage()
-        goobk.query(sys.argv[2].decode(ENCODING))
-    elif sys.argv[1] == "add":
+
+    cmd = args.pop(0)
+    if cmd == "query":
+        if len(args) != 1:
+            parser.error("incorrect number of arguments")
+        goobk.query(args[0].decode(ENCODING))
+    elif cmd == "add":
         goobk.add()
-    elif sys.argv[1] == "reload":
+    elif cmd == "reload":
         goobk.fetch()
         goobk.store()
     else:
-        usage()
+        parser.error('Command not recognized: %s' % cmd)
 
 if __name__ == '__main__':
     main()
