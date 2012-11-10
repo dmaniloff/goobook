6b7001059937d32b02171c9cc956e04192835453
Christer Sjöholm <hcs@furuvik.net>
2012-11-10 14:00:19 +0100

    Prepared 1.4 release

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ setup(name='goobook',
         Environment :: Console
         Operating System :: OS Independent
         Programming Language :: Python
-        Programming Language :: Python :: 2.5
         Programming Language :: Python :: 2.6
+        Programming Language :: Python :: 2.7
         Intended Audience :: End Users/Desktop
         License :: OSI Approved :: GNU General Public License (GPL)
         Topic :: Communications :: Email :: Address Book
